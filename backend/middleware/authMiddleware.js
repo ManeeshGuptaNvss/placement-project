429613e5d88272a9ed39ef5bf6e7f5ccf1bfa580
ManeeshGuptaNvss <48683390+ManeeshGuptaNvss@users.noreply.github.com>
2021-06-05 23:15:54 +0530

    Changed authmiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,18 +1,19 @@
 import jwt from 'jsonwebtoken'
+import AppError from '../utils/appError.js'
+import { promisify } from 'util'
 import asyncHandler from 'express-async-handler'
 import Student from '../models/studentModel.js'
 const protectStudent = asyncHandler(async (req, res, next) => {
-  let token
+  /*let token
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
       // Checking whether it is a bearer token or not
-      //
       token = req.headers.authorization.split(' ')[1]
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      console.log("decoded",decoded)
+      // console.log("decoded",decoded)
       req.student = await Student.findById(decoded.id).select('-password')
       next()
     } catch (error) {
@@ -24,7 +25,47 @@ const protectStudent = asyncHandler(async (req, res, next) => {
   if (!token) {
     res.status(401)
     throw new Error('Not authorized, no token')
+  }*/
+
+  // 1) Getting token and check of it's there
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1]
   }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    )
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+  // 3) Check if user still exists
+  const currentStudent = await Student.findById(decoded.id)
+  if (!currentStudent) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    )
+  }
+
+  // 4) Check if user changed password after the token was issued
+  if (currentStudent.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    )
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.student = currentStudent
+  next()
 })
 
 const admin = asyncHandler(async (req, res,next) => {
--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs'
-const studentSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'A name is required'], 
+      type: String,
+      required: [true, 'A name is required'],
     },
     roll: {
-        type: String,
-        required: [true, 'A student must have roll number'],
-        unique:true
+      type: String,
+      required: [true, 'A student must have roll number'],
+      unique: true,
     },
     yearOfJoining: {
-        type:String
+      type: String,
     },
     department: String,
     cgpa: String,
@@ -19,42 +20,78 @@ const studentSchema = new mongoose.Schema({
     tenthMarks: String,
     interMarks: String,
     diplomaMarks: String,
-    mobile:{
-        type: String,
-        required: [true, 'A student must have mobile number'],
-     
-    } ,
-    email: {
-        type: String,
-        required: [true, 'A student must have email number'],
-        
+    mobile: {
+      type: String,
+      required: [true, 'A student must have mobile number'],
     },
-    gender:String,
+    email: {
+      type: String,
+      required: [true, 'A student must have email number'],
+    },
+    gender: String,
     isAdmin: {
-        type: Boolean,
-    default:false},
-    isScrutinised:{
-        type: Boolean,
-    default:false},
+      type: Boolean,
+      default: false,
+    },
+    isScrutinised: {
+      type: Boolean,
+      default: false,
+    },
     password: {
-        type: String,
-        required:true
-    }   
-}, {
+      type: String,
+      required: true,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator: function (el) {
+          return el === this.password
+        },
+        message: 'Passwords are not the same!',
+      },
+    },
+    passwordChangedAt: Date,
+  },
+  {
     timestamps: true,
-    
-})
+  }
+)
+studentSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    )
 
+    return JWTTimestamp < changedTimestamp
+  }
+
+  // False means NOT changed
+  return false
+}
 studentSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword,this.password)
 }
 studentSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next()
-    }
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-    console.log('Pre middleware')
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) {
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  // console.log('Pre middleware')
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined
+  next()
+})
+studentSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next()
+
+  this.passwordChangedAt = Date.now() - 1000
+  next()
 })
 
 const Student = mongoose.model('Student', studentSchema)
